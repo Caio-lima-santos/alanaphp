c8e819d973892d807afdb7ffdb1292ab7adeb8b7
Caio-lima-santos <116364986+Caio-lima-santos@users.noreply.github.com>
2023-06-13 11:23:07 -0300

    Delete petshop directory

--- petshop/administrador/sla/sec.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-$_SESSION["Muser"]=$_REQUEST["Muser"];
-$_SESSION["Mfunc"]=$_REQUEST["Mfunc"];
-header("location: ../Aadm.php");
-?>
--- petshop/secretaria/sla/sec.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-$_SESSION["Muser"]=$_REQUEST["Muser"];
-echo"{$_REQUEST["Mfunc"]}";
-$_SESSION["Mfunc"]=$_REQUEST["Mfunc"];
-header("location: ../Asec.php");
-?>
